0b22932e01e427b146194cd24d66de03b4434a68
jules maregiano <jules.maregiano@gmail.com>
2017-09-14 21:54:30 +0200

    Ajout des chat rooms & du websocket comme un bourrin.

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -1,4 +1,8 @@
 class ChatRoomsController < ApplicationController
   def index
   end
+
+  def show
+    @chat_room = ChatRoom.includes(messages: :user).find(params[:id])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :babies, dependent: :destroy
+  has_many :messages, dependent: :destroy
 end
