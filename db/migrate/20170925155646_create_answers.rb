9c752fd518c3ef56f4e231ed4f103a46d77f524a
jules maregiano <jules.maregiano@gmail.com>
2017-09-25 18:14:39 +0200

    ajout des models de Comments et d'Answers, quelques vues, et un peu de controllers

---
+++ db/migrate/20170925155646_create_answers.rb
@@ -0,0 +1,11 @@
+class CreateAnswers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :answers do |t|
+      t.references :message, foreign_key: true
+      t.references :user, foreign_key: true
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
