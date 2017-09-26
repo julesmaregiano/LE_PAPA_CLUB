2fc22f51f381ebf1ea0497ac6b752aab735d2d5d
jules maregiano <jules.maregiano@gmail.com>
2017-09-26 17:47:03 +0200

    AutoScroll marche, mais maitnenant je dois le faire marcher en web socket..

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,8 +1,8 @@
-function scrollLastMessageIntoView() {
-  const messages = document.querySelectorAll('.message');
-  const lastMessage = messages[messages.length - 1];
+function scrollLastPostIntoView() {
+  const posts = document.querySelectorAll('.post');
+  const lastPost = posts[posts.length - 1];
 
-  if (lastMessage !== undefined) {
-    lastMessage.scrollIntoView();
-  }
-}
+  if (lastPost !== undefined) {
+    lastPost.scrollIntoView();
+  };
+};
