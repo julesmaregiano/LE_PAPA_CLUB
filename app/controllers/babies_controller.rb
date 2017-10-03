96fc07a19df6abd0c5f73e415c65241dd8b128c7
jules maregiano <jules.maregiano@gmail.com>
2017-10-03 21:12:54 +0200

    Ajout du bébé à l'edit de l'utilisateur

---
+++ app/controllers/babies_controller.rb
@@ -0,0 +1,16 @@
+class BabiesController < ApplicationController
+
+  def update
+    @user = current_user
+    @baby = @user.babies.first
+    if @baby.update(baby_params)
+      render "users/edit"
+    end
+  end
+
+  private
+
+  def baby_params
+    params.require(:baby).permit(:name, :weight, :date_of_birth)
+  end
+end
