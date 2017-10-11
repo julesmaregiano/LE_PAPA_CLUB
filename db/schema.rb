5e1d14bdddeefc24f9433c1d7eaeb74dcc60a6bc
jules maregiano <jules.maregiano@gmail.com>
2017-10-11 10:19:06 +0200

    Ajout de l'authentification Facebook.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171001192853) do
+ActiveRecord::Schema.define(version: 20171011081429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -123,6 +123,11 @@ ActiveRecord::Schema.define(version: 20171001192853) do
     t.date "date_of_birth"
     t.integer "couple_situation"
     t.integer "role"
+    t.string "provider"
+    t.string "uid"
+    t.string "facebook_picture_url"
+    t.string "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
