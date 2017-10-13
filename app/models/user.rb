bf5a5946c4734de0653a6e7052cf51224cf54fe4
jules maregiano <jules.maregiano@gmail.com>
2017-10-13 13:08:31 +0200

    migration pour ajouter des infos dans la fiche produit

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook]
 
-
   has_many :babies, dependent: :destroy
   has_many :messages, dependent: :destroy
   has_many :posts, dependent: :destroy
