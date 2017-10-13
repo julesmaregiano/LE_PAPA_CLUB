0b1b3417d34550275d48d1bb826621b03b22daf4
Lea Durand <leadurand.pro@outlook.com>
2017-10-13 16:35:48 +0200

    fixed like bug

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ApplicationRecord
   acts_as_votable
   validates :content, presence: true, allow_blank: false
-  after_create :broadcast_post
   has_many :likes
   has_many :messages
   belongs_to :user
@@ -10,16 +9,6 @@ class Post < ApplicationRecord
 
   def from?(some_user)
     user == some_user
-  end
-
-  def broadcast_post
-    ActionCable.server.broadcast("chat_room_#{chat_room.id}", {
-      post_partial: ApplicationController.renderer.render(
-        partial: "posts/post",
-        locals: { post: self, user_is_posts_author: false }
-      ),
-      current_user_id: user.id
-    })
   end
 
 end
