3ebcb21258c60dce1b2732e1e776128b0886eda7
jules maregiano <jules.maregiano@gmail.com>
2017-10-15 21:45:55 +0200

    Amélioration CSS pour la page Articles

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.all.where(type: params[:type_id])
     @types = Type.first(3)
   end
 end
