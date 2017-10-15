7627735767a5a095d7b70a783da013c8753a8f30
jules maregiano <jules.maregiano@gmail.com>
2017-10-15 14:30:45 +0200

    Amélioration page Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,5 +6,6 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
     @comments = @article.comments
+    @random_articles = Article.all.sample(5)
   end
 end
