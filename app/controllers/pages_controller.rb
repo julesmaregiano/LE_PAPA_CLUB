368705c17f97da8cb9db42b51a6984a1124a70f8
jules maregiano <jules.maregiano@gmail.com>
2017-11-01 15:06:24 +0100

    ajout de routes pour une homepage + squelette de la home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    render :navbar => false
+  end
+
+  def categories
     @categories = Type.all.last(3)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root to: 'pages#home'
+  get 'categories', to: 'pages#categories'
   mount Attachinary::Engine => "/attachinary"
   mount ActionCable.server => "/cable"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
