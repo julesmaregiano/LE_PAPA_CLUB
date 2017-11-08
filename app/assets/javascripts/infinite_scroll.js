6bd26c05f68535c47b9bef3becb19e1123181001
jules maregiano <jules.maregiano@gmail.com>
2017-11-08 18:52:11 +0100

    Autoload marche sur les articles

--- app/assets/javascripts/infinite_scroll.js
+++ app/assets/javascripts/infinite_scroll.js
@@ -16,3 +16,22 @@ $(document).on('ready page:load', function () {
     });
   }
 });
+
+$(document).on('ready page:load', function () {
+  var isLoading = false;
+  if ($('#infinite-articles').size() > 0) {
+    $('.layout').on('scroll', function() {
+      var more_articles_url = $('.pagination a.next_page').attr('href');
+      console.log($('.articles').height() - $('.layout').scrollTop())
+      if (!isLoading && more_articles_url && ($('.articles').height() - $('.layout').scrollTop()) < 600 ) {
+        console.log('Ca va charger!');
+        isLoading = true;
+        $.getScript(more_articles_url).done(function (data,textStatus,jqxhr) {
+          isLoading = false;
+        }).fail(function() {
+          isLoading = false;
+        });
+      }
+    });
+  }
+});
--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,7 @@ class Article < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_attachment :photo
+  self.per_page = 5
   def comments
     Comment.all.where(article: self)
   end
